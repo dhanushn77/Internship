cbdf1987b8fc7a3c0a4ec78e7f8c2c0ebf9a47c6
dhanushn77 <dhanushn2608@gmail.com>
2025-01-10 16:14:47 +0530

    Create Simpleintrest.py

---
+++ Simpleintrest.py
@@ -0,0 +1,11 @@
+
+#To calculate the simple intrest 
+
+p=int(input("please enter principle: "))
+t=int(input("please enter the time: "))
+r=int(input("pls enter the rate: "))
+
+simp_int=(p*t*r)/100
+
+
+print(simp_int)
